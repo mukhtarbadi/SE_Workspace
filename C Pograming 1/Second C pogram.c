24f5a901808a5e5a03fd4d0e0ff701f8fa0c8301
mukhtarbadi <127752593+mukhtarbadi@users.noreply.github.com>
2025-06-13 10:08:58 +0530

    Add files via upload

---
+++ C Pograming 1/Second C pogram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Your Name : Mukhtar");
+    printf("\t Your Age : 21");
+    printf("\t Your City : Wankaner");
+  
+   getch(); 
+}
---
+++ C Pograming 1/third C pogram.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\n Your Name : Mukhtar");
+    printf("\n Your Age : 21");
+    printf("\n Your City : Wankaner");
+ 
+   getch(); 
+}
